4128092e45c2037f8de1196c3dfeef28e24c5d7b
Julie S Mike <juliesmike@gmail.com>
2017-03-19 17:42:54 -0500

    new version dinder app

---
+++ app/components/sideMenu.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Text,
+  View,
+  Button
+} from 'native-base'
+
+export default class SideMenu extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return null
+  }
+}
---
+++ index.ios.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+ import React, { Component } from 'react';
+ import {
+   AppRegistry,
+ } from 'react-native';
+ import AppContainer from './app/AppContainer'
+ export default class dinder2 extends Component {
+   render() {
+     return (
+       <AppContainer/>
+     );
+   }
+ }
+
+ AppRegistry.registerComponent('dinder2', () => dinder2);
