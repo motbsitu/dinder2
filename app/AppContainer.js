3d9af90c44de83a1386df45568c705d7d9b4aa95
Julie S Mike <juliesmike@gmail.com>
2017-03-21 15:45:27 -0500

    splash screen

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -4,13 +4,22 @@ import { Navigator } from 'react-native'
 
 import SideMenu from './components/sideMenu'
 
+import SettingsStore from './stores/settingsStore'
+import SplashScene from './scenes/splashScene'
+
+import theme from './theme/base-theme'
+
+const settings = new SettingsStore()
+
 export default class AppContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
       toggled: false,
-      store: {},
-      theme: null
+      store: {
+        settings: settings
+      },
+      theme: theme
     }
   }
   toggleDrawer() {
@@ -23,7 +32,10 @@ export default class AppContainer extends Component {
     this.setState({toggled: false})
   }
   renderScene(route, navigator) {
-    switch(route) {
+    switch(route.title) {
+      case 'Splash':{
+        return <SplashScene {...route.passProps} navigator={navigator}/>
+      }
       default: {
         return null
       }
@@ -46,6 +58,14 @@ export default class AppContainer extends Component {
             ref={(ref) => this._navigator = ref}
             configureScene={this.configureScene.bind(this)}
             renderScene={this.renderScene.bind(this)}
+            initialRoute={{
+              title: "Splash",
+              passProps: {
+                stores: this.state.store,
+                toggleDrawer: this.toggleDrawer.bind(this),
+                theme: this.state.theme
+              }
+            }}
             />
         </Drawer>
     )
---
+++ app/stores/settingsStore.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase'
+import MobxFirebaseStore from 'mobx-firebase-store'
+
+const config = {
+  apiKey: "AIzaSyCdHzKi2wkAumosbEiMO79dxCPiM9iQMhA",
+    authDomain: "dinder-5eaef.firebaseapp.com",
+    databaseURL: "https://dinder-5eaef.firebaseio.com",
+    storageBucket: "dinder-5eaef.appspot.com",
+    messagingSenderId: "842791904999"
+}
+export default class SettingsStore extends MobxFirebaseStore {
+  constructor(){
+    firebase.initializeApp(config)
+    super(firebase.database().ref())
+
+    this.splashTime = 5000
+    this.splashImg = require('../../images/splash.jpg')
+  }
+  get SplashTime(){
+    return this.splashTime
+  }
+  get SplashImg(){
+    return this.splashImg
+  }
+}
